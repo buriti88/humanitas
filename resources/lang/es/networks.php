d44dd63dce4215d39c6fba64d610417bd993ed12
aig_mburitica <mburitica@aigroup.com.co>
2021-12-02 08:20:59 -0500

    Add: Se crea archivo del lenguaje

---
+++ resources/lang/es/networks.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    //Actions
+    'created' => 'El Empleado :name ha sido creado con éxito.',
+    'updated' => 'El Empleado :name ha sido actualizado con éxito.',
+    'deleted' => 'El Empleado :name ha sido eliminado con éxito.',
+    'failed' => 'Error al :action del empleado',
+
+    //Tags
+    'name' => 'Nombre',
+    'title' => 'Cargo',
+];
