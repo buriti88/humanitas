b5779a4e0adbab5938ebde3163ea79c614d7db91
marlonhanner <marlonhannerh@gmail>
2021-12-13 16:01:15 -0500

    fix: se agregan campos a ventana detalles

--- resources/views/employees/detail.blade.php
+++ resources/views/employees/detail.blade.php
@@ -63,7 +63,75 @@
                                         {{$employee->expedition_date ?? '' ?? ''}}
                                     </td>
                                 </tr>
+
+                                <td><b>@lang('employees.date_birth')</b></td>
+                                <td class="background_color">
+                                    {{$employee->date_birth ?? '' ?? ''}}
+                                </td>
+
+                                <td><b>@lang('employees.gender_id')</b></td>
+                                <td class="background_color">
+                                    {{$employee->gender-> option ?? ''}}
+                                </td>
+
+                                <td><b>@lang('employees.rh_id')</b></td>
+                                <td class="background_color">
+                                    {{$employee->rh-> option ?? ''}}
+                                </td>
+
+                                <td><b>@lang('employees.telephone')</b></td>
+                                <td class="background_color">
+                                    {{$employee->telephone ?? '' ?? ''}}
+                                </td>
+                               
                             </tbody>
+
+                            <tbody>
+                                <td><b>@lang('employees.email')</b></td>
+                                <td class="background_color">
+                                    {{$employee->email ?? '' ?? ''}}
+                                </td>
+
+                                <td><b>@lang('employees.address')</b></td>
+                                <td class="background_color">
+                                    {{$employee->address ?? '' ?? ''}}
+                                </td>
+
+                                <td><b>@lang('employees.neighborhood')</b></td>
+                                <td class="background_color">
+                                    {{$employee->neighborhood ?? '' ?? ''}}
+                                </td>
+
+                                <td><b>@lang('employees.municipality_id')</b></td>
+                                <td class="background_color">
+                                    {{$employee->municipality-> option ?? ''}}
+                                </td>
+                            </tbody>
+
+                            <tbody>
+                                <td><b>@lang('employees.type_housing_id')</b></td>
+                                <td class="background_color">
+                                    {{$employee->type_housing-> option ?? ''}}
+                                </td>
+
+                                <td><b>@lang('employees.stratum_id')</b></td>
+                                <td class="background_color">
+                                    {{$employee->stratum-> option ?? ''}}
+                                </td>
+
+                                <td><b>@lang('employees.marital_status_id')</b></td>
+                                <td class="background_color">
+                                    {{$employee->marital_status_id-> option ?? ''}}
+                                </td>
+
+                                <td><b>@lang('employees.number_children')</b></td>
+                                <td class="background_color">
+                                    {{$employee->number_children ?? '' ?? ''}}
+                                </td>
+
+                            </tbody>
+                        
+                        
                         </table>
                     </div>
 
@@ -96,6 +164,96 @@
                                     </td>
                                 </tr>
                             </tbody>
+
+                            <tbody>
+                                <td><b>@lang('employees.health_id')</b></td>
+                                <td class="background_color">
+                                    {{$employee->health-> option ?? ''}}
+                                </td>
+    
+                                <td><b>@lang('employees.pension_id')</b></td>
+                                <td class="background_color">
+                                    {{$employee->pension-> option ?? ''}}
+                                </td>
+    
+                                <td><b>@lang('employees.arl_id')</b></td>
+                                <td class="background_color">
+                                    {{$employee->arl-> option ?? ''}}
+                                </td>
+                                
+                                <td><b>@lang('employees.advan_life_support')</b></td>
+                                <td class="background_color">
+                                    {{$employee->advan_life_support ?? '' ?? ''}}
+                                </td>
+
+                            </tbody>
+
+                            <tbody>
+                                <td><b>@lang('employees.account_type_id')</b></td>
+                                <td class="background_color">
+                                    {{$employee->account_type-> option ?? ''}}
+                                </td>
+
+                                <td><b>@lang('employees.bank_id')</b></td>
+                                <td class="background_color">
+                                    {{$employee->bank-> option ?? ''}}
+                                </td>
+
+                                <td><b>@lang('employees.account_number')</b></td>
+                                <td class="background_color">
+                                    {{$employee->account_number ?? '' ?? ''}}
+                                </td>
+
+                                <td><b>@lang('employees.certificate_degrees')</b></td>
+                                <td class="background_color">
+                                    {{$employee->certificate_degrees ?? '' ?? ''}}
+                                </td>
+                            </tbody>
+                            
+                        <tbody>
+                            <td><b>@lang('employees.title_verification')</b></td>
+                            <td class="background_color">
+                                {{$employee->title_verification ?? '' ?? ''}}
+                            </td>
+
+                             <td><b>@lang('employees.professional_card')</b></td>
+                            <td class="background_color">
+                                {{$employee->professional_card ?? '' ?? ''}}
+                            </td>
+
+                               <td><b>@lang('employees.function_manual')</b></td>
+                            <td class="background_color">
+                                {{$employee->function_manual ?? '' ?? ''}}
+                            </td>
+
+                               <td><b>@lang('employees.resolution_rethus')</b></td>
+                            <td class="background_color">
+                                {{$employee->resolution_rethus ?? '' ?? ''}}
+                            </td>
+
+                        </tbody>
+
+                        <tbody>
+                            <td><b>@lang('employees.court_ethics_certific')</b></td>
+                            <td class="background_color">
+                                {{$employee->court_ethics_certific ?? '' ?? ''}}
+                            </td>
+
+                               <td><b>@lang('employees.card_protect_validity')</b></td>
+                            <td class="background_color">
+                                {{$employee->card_protect_validity ?? '' ?? ''}}
+                            </td>
+
+                               <td><b>@lang('employees.occupational_exam')</b></td>
+                            <td class="background_color">
+                                {{$employee->occupational_exam ?? '' ?? ''}}
+                            </td>
+
+                               <td><b>@lang('employees.observation')</b></td>
+                            <td class="background_color">
+                                {{$employee->observation ?? '' ?? ''}}
+                            </td>
+                        </tbody>
                         </table>
                     </div>
                 </div>
