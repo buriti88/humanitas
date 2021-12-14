d4ee9b3ed6d391d6f7009d2204868b3778995091
aig_mburitica <mburitica@aigroup.com.co>
2021-12-14 15:06:22 -0500

    Add: Campos de búsqueda

--- resources/views/employees/_search.blade.php
+++ resources/views/employees/_search.blade.php
@@ -1,30 +1,46 @@
 <form class="form-inline" action="{{ route('employees.index') }}" method="get" role="search">
     <input type="hidden" name="per_page" value="{{$employees->perPage()}}" />
-    <div class="col-sm-12 col-lg-4">
-        <label>@lang('employees.name')</label>
+
+    <div class="col-sm-12 col-md-3">
+        <label>@lang('employees.identification_card')</label>
         <div class="input-group input-group-sm mb-2">
             <div class="input-group-prepend">
-                <div class="input-group-text"><i class="fas fa-user"></i></div>
+                <div class="input-group-text"><i class="fa fa-hashtag"></i></div>
             </div>
-            <input type="text" class="form-control" name="q[name]" value="{{$search['name'] ?? ''}}"
-                placeholder="@lang('employees.name')" autocomplete="off">
+            <input type="text" class="form-control" name="q[identification_card]"
+                value="{{$search['identification_card'] ?? ''}}" placeholder="@lang('employees.identification_card')"
+                autocomplete="off">
         </div>
     </div>
-    <div class="col-sm-12 col-lg-4">
+
+    <div class="col-sm-12 col-md-3">
+        <label>@lang('employees.date_birth')</label>
+        <div class="input-group input-group-sm mb-2">
+            <div class="input-group-prepend">
+                <div class="input-group-text"><i class="far fa-calendar-check"></i></div>
+            </div>
+            <input type="text" class="form-control datepicker" name="q[date_birth]"
+                value="{{$search['date_birth'] ?? ''}}" placeholder="@lang('employees.date_birth')"
+                data-date-format="{{config('app.js_date_format')}}" autocomplete="off">
+        </div>
+    </div>
+
+    <div class="col-sm-12 col-md-3">
         <label>@lang('users.status')</label>
         <div class="input-group input-group-sm mb-2">
             <select class="form-control-sm select2 w-100" name="q[status]">
                 <option value="">@lang('users.status')</option>
-                <option value="1" {{($search['active'] ?? '') == '1' ? 'selected' : ''}}>@lang('base_lang.active')
+                <option value="1" {{($search['status'] ?? '' )=='1' ? 'selected' : '' }}>@lang('base_lang.active')
                 </option>
-                <option value="0" {{($search['active'] ?? '') == '0' ? 'selected' : ''}}>@lang('base_lang.inactive')
+                <option value="0" {{($search['status'] ?? '' )=='0' ? 'selected' : '' }}>@lang('base_lang.inactive')
                 </option>
             </select>
         </div>
     </div>
+
     <div class="col-sm-12 col-lg-4 mt-3">
         <button type="submit" class="btn btn-sm btn-primary mb-1">@lang('base_lang.search')</button>
-        <a href="{{url('admin/employees?q[]')}}" class="btn btn-sm btn-primary mb-1">
+        <a href="{{url('employees?q[]')}}" class="btn btn-sm btn-primary mb-1">
             @lang('base_lang.clear_search')
         </a>
     </div>
