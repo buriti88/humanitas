1e60636bf9b4c3c15816fda5abf748a2dc323a44
marlonhanner <marlonhannerh@gmail>
2021-12-15 14:15:36 -0500

    fix: se ingresan campos

--- resources/views/employees/_search.blade.php
+++ resources/views/employees/_search.blade.php
@@ -13,6 +13,43 @@
         </div>
     </div>
 
+       <div class="col-sm-12 col-md-3">
+        <label>@lang('employees.name')</label>
+        <div class="input-group input-group-sm mb-2">
+            <div class="input-group-prepend">
+                <div class="input-group-text"><i class="fa fa-hashtag"></i></div>
+            </div>
+            <input type="text" class="form-control" name="q[name]"
+                value="{{$search['name'] ?? ''}}" placeholder="@lang('employees.name')"
+                autocomplete="off">
+        </div>
+    </div>
+
+       <div class="col-sm-12 col-md-3">
+        <label>@lang('employees.last_name')</label>
+        <div class="input-group input-group-sm mb-2">
+            <div class="input-group-prepend">
+                <div class="input-group-text"><i class="fa fa-hashtag"></i></div>
+            </div>
+            <input type="text" class="form-control" name="q[last_name]"
+                value="{{$search['last_name'] ?? ''}}" placeholder="@lang('employees.last_name')"
+                autocomplete="off">
+        </div>
+    </div>
+
+   <div class="col-sm-12 col-md-3">
+        <label>@lang('employees.expedition_date')</label>
+        <div class="input-group input-group-sm mb-2">
+            <div class="input-group-prepend">
+                <div class="input-group-text"><i class="far fa-calendar-check"></i></div>
+            </div>
+            <input type="text" class="form-control datepicker" name="q[expedition_date]"
+                value="{{$search['expedition_date'] ?? ''}}" placeholder="@lang('employees.expedition_date')"
+                data-date-format="{{config('app.js_date_format')}}" autocomplete="off">
+        </div>
+    </div>
+
+
     <div class="col-sm-12 col-md-3">
         <label>@lang('employees.date_birth')</label>
         <div class="input-group input-group-sm mb-2">
@@ -24,6 +61,7 @@
                 data-date-format="{{config('app.js_date_format')}}" autocomplete="off">
         </div>
     </div>
+
 
     <div class="col-sm-12 col-md-3">
         <label>@lang('employees.title_id')</label>
@@ -39,18 +77,130 @@
         </div>
     </div>
 
+
     <div class="col-sm-12 col-md-3">
-        <label>@lang('users.status')</label>
+        <label>@lang('employees.email')</label>
         <div class="input-group input-group-sm mb-2">
-            <select class="form-control-sm select2 w-100" name="q[status]">
-                <option value="">@lang('users.status')</option>
-                <option value="1" {{($search['status'] ?? '' )=='1' ? 'selected' : '' }}>@lang('base_lang.active')
-                </option>
-                <option value="0" {{($search['status'] ?? '' )=='0' ? 'selected' : '' }}>@lang('base_lang.inactive')
-                </option>
-            </select>
+            <div class="input-group-prepend">
+                <div class="input-group-text"><i class="fa fa-hashtag"></i></div>
+            </div>
+            <input type="text" class="form-control" email="q[email]"
+                value="{{$search['email'] ?? ''}}" placeholder="@lang('employees.email')"
+                autocomplete="off">
         </div>
     </div>
+
+    <div class="col-sm-12 col-md-3">
+        <label>@lang('employees.telephone')</label>
+        <div class="input-group input-group-sm mb-2">
+            <div class="input-group-prepend">
+                <div class="input-group-text"><i class="fa fa-hashtag"></i></div>
+            </div>
+            <input type="text" class="form-control" telephone="q[telephone]"
+                value="{{$search['telephone'] ?? ''}}" placeholder="@lang('employees.telephone')"
+                autocomplete="off">
+        </div>
+    </div>
+
+    <div class="col-sm-12 col-md-3">
+        <label>@lang('employees.address')</label>
+        <div class="input-group input-group-sm mb-2">
+            <div class="input-group-prepend">
+                <div class="input-group-text"><i class="fa fa-hashtag"></i></div>
+            </div>
+            <input type="text" class="form-control" address="q[address]"
+                value="{{$search['address'] ?? ''}}" placeholder="@lang('employees.address')"
+                autocomplete="off">
+        </div>
+    </div>
+
+    <div class="col-sm-12 col-md-3">
+        <label>@lang('employees.neighborhood')</label>
+        <div class="input-group input-group-sm mb-2">
+            <div class="input-group-prepend">
+                <div class="input-group-text"><i class="fa fa-hashtag"></i></div>
+            </div>
+            <input type="text" class="form-control" neighborhood="q[neighborhood]"
+                value="{{$search['neighborhood'] ?? ''}}" placeholder="@lang('employees.neighborhood')"
+                autocomplete="off">
+        </div>
+    </div>
+
+    
+    <div class="col-sm-12 col-md-3">
+        <label>@lang('employees.number_children')</label>
+        <div class="input-group input-group-sm mb-2">
+            <div class="input-group-prepend">
+                <div class="input-group-text"><i class="fa fa-hashtag"></i></div>
+            </div>
+            <input type="text" class="form-control" number_children="q[number_children]"
+                value="{{$search['number_children'] ?? ''}}" placeholder="@lang('employees.number_children')"
+                autocomplete="off">
+        </div>
+    </div>
+
+    <div class="col-sm-12 col-md-3">
+        <label>@lang('employees.account_number')</label>
+        <div class="input-group input-group-sm mb-2">
+            <div class="input-group-prepend">
+                <div class="input-group-text"><i class="fa fa-hashtag"></i></div>
+            </div>
+            <input type="text" class="form-control" account_number="q[account_number]"
+                value="{{$search['account_number'] ?? ''}}" placeholder="@lang('employees.account_number')"
+                autocomplete="off">
+        </div>
+    </div>
+
+    <div class="col-sm-12 col-md-3">
+        <label>@lang('employees.advan_life_support')</label>
+        <div class="input-group input-group-sm mb-2">
+            <div class="input-group-prepend">
+                <div class="input-group-text"><i class="far fa-calendar-check"></i></div>
+            </div>
+            <input type="text" class="form-control datepicker" name="q[advan_life_support]"
+                value="{{$search['advan_life_support'] ?? ''}}" placeholder="@lang('employees.advan_life_support')"
+                data-date-format="{{config('app.js_date_format')}}" autocomplete="off">
+        </div>
+    </div>
+
+    <div class="col-sm-12 col-md-3">
+        <label>@lang('employees.date_admission')</label>
+        <div class="input-group input-group-sm mb-2">
+            <div class="input-group-prepend">
+                <div class="input-group-text"><i class="far fa-calendar-check"></i></div>
+            </div>
+            <input type="text" class="form-control datepicker" name="q[date_admission]"
+                value="{{$search['date_admission'] ?? ''}}" placeholder="@lang('employees.date_admission')"
+                data-date-format="{{config('app.js_date_format')}}" autocomplete="off">
+        </div>
+    </div>
+
+    <div class="col-sm-12 col-md-3">
+        <label>@lang('employees.date_end')</label>
+        <div class="input-group input-group-sm mb-2">
+            <div class="input-group-prepend">
+                <div class="input-group-text"><i class="far fa-calendar-check"></i></div>
+            </div>
+            <input type="text" class="form-control datepicker" name="q[date_end]"
+                value="{{$search['date_end'] ?? ''}}" placeholder="@lang('employees.date_end')"
+                data-date-format="{{config('app.js_date_format')}}" autocomplete="off">
+        </div>
+    </div>
+
+
+
+
+
+ 
+
+    
+
+
+  
+
+     
+
+       
 
     <div class="col-sm-12 col-lg-4 mt-3">
         <button type="submit" class="btn btn-sm btn-primary mb-1">@lang('base_lang.search')</button>
