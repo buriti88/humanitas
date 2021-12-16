fbc2e63062c3b6527d87a4d13663d1ff04d519a7
marlonhanner <marlonhannerh@gmail>
2021-12-16 08:33:00 -0500

    fix: cambio en los campos de busqueda

--- resources/views/employees/_search.blade.php
+++ resources/views/employees/_search.blade.php
@@ -1,17 +1,7 @@
 <form class="form-inline" action="{{ route('employees.index') }}" method="get" role="search">
     <input type="hidden" name="per_page" value="{{$employees->perPage()}}" />
 
-    <div class="col-sm-12 col-md-3">
-        <label>@lang('employees.identification_card')</label>
-        <div class="input-group input-group-sm mb-2">
-            <div class="input-group-prepend">
-                <div class="input-group-text"><i class="fa fa-hashtag"></i></div>
-            </div>
-            <input type="text" class="form-control" name="q[identification_card]"
-                value="{{$search['identification_card'] ?? ''}}" placeholder="@lang('employees.identification_card')"
-                autocomplete="off">
-        </div>
-    </div>
+  
 
        <div class="col-sm-12 col-md-3">
         <label>@lang('employees.name')</label>
@@ -37,6 +27,18 @@
         </div>
     </div>
 
+    <div class="col-sm-12 col-md-3">
+        <label>@lang('employees.identification_card')</label>
+        <div class="input-group input-group-sm mb-2">
+            <div class="input-group-prepend">
+                <div class="input-group-text"><i class="fa fa-hashtag"></i></div>
+            </div>
+            <input type="text" class="form-control" name="q[identification_card]"
+                value="{{$search['identification_card'] ?? ''}}" placeholder="@lang('employees.identification_card')"
+                autocomplete="off">
+        </div>
+    </div>
+
    <div class="col-sm-12 col-md-3">
         <label>@lang('employees.expedition_date')</label>
         <div class="input-group input-group-sm mb-2">
@@ -48,7 +50,6 @@
                 data-date-format="{{config('app.js_date_format')}}" autocomplete="off">
         </div>
     </div>
-
 
     <div class="col-sm-12 col-md-3">
         <label>@lang('employees.date_birth')</label>
@@ -62,7 +63,6 @@
         </div>
     </div>
 
-
     <div class="col-sm-12 col-md-3">
         <label>@lang('employees.title_id')</label>
         <div class="input-group input-group-sm mb-2">
@@ -74,92 +74,6 @@
                 </option>
                 @endforeach
             </select>
-        </div>
-    </div>
-
-
-    <div class="col-sm-12 col-md-3">
-        <label>@lang('employees.email')</label>
-        <div class="input-group input-group-sm mb-2">
-            <div class="input-group-prepend">
-                <div class="input-group-text"><i class="fa fa-hashtag"></i></div>
-            </div>
-            <input type="text" class="form-control" email="q[email]"
-                value="{{$search['email'] ?? ''}}" placeholder="@lang('employees.email')"
-                autocomplete="off">
-        </div>
-    </div>
-
-    <div class="col-sm-12 col-md-3">
-        <label>@lang('employees.telephone')</label>
-        <div class="input-group input-group-sm mb-2">
-            <div class="input-group-prepend">
-                <div class="input-group-text"><i class="fa fa-hashtag"></i></div>
-            </div>
-            <input type="text" class="form-control" telephone="q[telephone]"
-                value="{{$search['telephone'] ?? ''}}" placeholder="@lang('employees.telephone')"
-                autocomplete="off">
-        </div>
-    </div>
-
-    <div class="col-sm-12 col-md-3">
-        <label>@lang('employees.address')</label>
-        <div class="input-group input-group-sm mb-2">
-            <div class="input-group-prepend">
-                <div class="input-group-text"><i class="fa fa-hashtag"></i></div>
-            </div>
-            <input type="text" class="form-control" address="q[address]"
-                value="{{$search['address'] ?? ''}}" placeholder="@lang('employees.address')"
-                autocomplete="off">
-        </div>
-    </div>
-
-    <div class="col-sm-12 col-md-3">
-        <label>@lang('employees.neighborhood')</label>
-        <div class="input-group input-group-sm mb-2">
-            <div class="input-group-prepend">
-                <div class="input-group-text"><i class="fa fa-hashtag"></i></div>
-            </div>
-            <input type="text" class="form-control" neighborhood="q[neighborhood]"
-                value="{{$search['neighborhood'] ?? ''}}" placeholder="@lang('employees.neighborhood')"
-                autocomplete="off">
-        </div>
-    </div>
-
-    
-    <div class="col-sm-12 col-md-3">
-        <label>@lang('employees.number_children')</label>
-        <div class="input-group input-group-sm mb-2">
-            <div class="input-group-prepend">
-                <div class="input-group-text"><i class="fa fa-hashtag"></i></div>
-            </div>
-            <input type="text" class="form-control" number_children="q[number_children]"
-                value="{{$search['number_children'] ?? ''}}" placeholder="@lang('employees.number_children')"
-                autocomplete="off">
-        </div>
-    </div>
-
-    <div class="col-sm-12 col-md-3">
-        <label>@lang('employees.account_number')</label>
-        <div class="input-group input-group-sm mb-2">
-            <div class="input-group-prepend">
-                <div class="input-group-text"><i class="fa fa-hashtag"></i></div>
-            </div>
-            <input type="text" class="form-control" account_number="q[account_number]"
-                value="{{$search['account_number'] ?? ''}}" placeholder="@lang('employees.account_number')"
-                autocomplete="off">
-        </div>
-    </div>
-
-    <div class="col-sm-12 col-md-3">
-        <label>@lang('employees.advan_life_support')</label>
-        <div class="input-group input-group-sm mb-2">
-            <div class="input-group-prepend">
-                <div class="input-group-text"><i class="far fa-calendar-check"></i></div>
-            </div>
-            <input type="text" class="form-control datepicker" name="q[advan_life_support]"
-                value="{{$search['advan_life_support'] ?? ''}}" placeholder="@lang('employees.advan_life_support')"
-                data-date-format="{{config('app.js_date_format')}}" autocomplete="off">
         </div>
     </div>
 
